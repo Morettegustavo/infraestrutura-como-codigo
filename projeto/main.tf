0a86e883e51a091213f7810327f6194906cd04ca
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2022-12-28 12:51:14 -0300

    main.js

---
+++ projeto/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0574da719dca65348"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
